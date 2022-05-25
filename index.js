7550cb67e8ce6194a7cbbd7557eaaa58af11ff34
Adrian Alberto <adrianalberto1986@gmail.com>
2022-05-24 22:21:40 -0500

    commit tree 8c82d03f20e24424a41a0ca67ae816ee31379cf3

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(name){
+    const newNames = []
+    for (let x = 0; x < name.length; x++){
+        newNames[x] = `Thank you, ${name[x]}, for the wonderful surprise gift!`
+    }
+    return newNames
+}
+
+function countDown(num){
+    for (let x = num; x >= 0; x--){
+        console.log(x)
+    }
+}
